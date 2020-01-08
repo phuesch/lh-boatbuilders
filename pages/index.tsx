f60c2714bd75f7a9ff45c00eccf994e004ef6fab
pauher <pauline.hering@sinnerschrader.com>
2020-01-08 15:07:40 +0100

    feat: footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,37 @@ const Header = styled.div`
   font-family: "Verdana";
   padding: 20px;
 `
+const Welcome = styled.div`
+  display: flex;
+  align-items: center;
+  align-content: center;
+  flex-direction: column;
+  margin: 230px;
+  font-family: "Verdana";
+`
+
+const Footer = styled.div`
+  display: flex;
+  background-image: url('/header.jpg');
+  background-size: cover;
+  background-position: bottom;
+  min-height: 100px;
+  align-self: baseline; 
+  align-items: column;
+  font-family: "Verdana";
+  color: #fdfefe;
+
+`
+const Contact = styled.div`
+  align-items: row;
+  padding:   0px 100px 0px 20px;
+  font-size: 15px;
+`
+const OpeningHours = styled.div`
+  align-items: row;  
+  font-size: 15px;
+`
+const NavigationBar = styled.div``
 
 
 const Home = () => (
@@ -25,81 +56,27 @@ const Home = () => (
     </Head>
     <Header>
       <p>LH Boatbuilders</p>
+      <link rel="stylesheet" href="/index.ts"/>
     </Header>
 
-    <Nav />
-
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
+    <Welcome>
+    <h1 className="title">Willkommen</h1>
       <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
+        Diese Website befindet sich im Aufbau. 
+      </p> 
+    </Welcome>
 
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
-    </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
+    <Footer>
+      <Contact>
+        <p>Adresse folgt</p>
+        <p>Telefonnummer</p>
+      </Contact>
+      <OpeningHours>
+        <p>Öffnungszeiten</p>
+        <p>Montag bis Freitag:  10:00 - 18:00 Uhr</p>
+        <p>Sonntag:             10:00 - 14:00 Uhr</p>
+      </OpeningHours>
+    </Footer>
   </div>
 )
 
