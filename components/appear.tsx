7b2e08e638cf61ff50ba48292526e586a931babf
pauher <pauline.hering@sinnerschrader.com>
2020-03-12 15:19:07 +0100

    feat: add transition to appear

--- components/appear.tsx
+++ components/appear.tsx
@@ -3,14 +3,22 @@ import styled from "styled-components";
 
 interface GhostiProps {
   isGhostin: boolean;
+  transitionDuration?: number;
+  transitionDelay?: number;
 }
 const Ghosti = styled.div<GhostiProps>`
   opacity: ${props => (props.isGhostin ? 0 : 1)};
   transform: translateX(${props => (props.isGhostin ? "50px" : 0)});
-  transition: all 2.5s;
+  transition: all ${props => props.transitionDuration}s;
+  transition-delay: ${props => props.transitionDelay}s;
 `;
 
-export const Appear: FC = props => {
+interface AppearProps {
+  transitionDuration?: number;
+  transitionDelay?: number;
+}
+
+export const Appear: FC<AppearProps> = props => {
   const [visible, setVisible] = useState(false);
   const ghostiRef = useRef<HTMLDivElement>();
 
@@ -33,7 +41,12 @@ export const Appear: FC = props => {
   }, []);
 
   return (
-    <Ghosti ref={ghostiRef} isGhostin={!visible}>
+    <Ghosti
+      ref={ghostiRef}
+      isGhostin={!visible}
+      transitionDuration={props.transitionDuration || 2.5}
+      transitionDelay={props.transitionDelay}
+    >
       {props.children}
     </Ghosti>
   );
